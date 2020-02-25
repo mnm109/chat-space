3e9b38f6e412f431f6fe00ca3867f896cf3d9f56
mnm109 <61045251+mnm109@users.noreply.github.com>
2020-02-26 00:26:32 +0900

    messages edit 2nd
    メッセージが保存できた場合、できなかった場合

--- spec/models/messages_spec.rb
+++ spec/models/messages_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
+  describe '#create' do
+    context 'can save' do
+    end
+  end
+
+    context 'can not save' do
+    end
 end
 
