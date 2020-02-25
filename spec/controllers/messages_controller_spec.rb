7e607427a933b8e0050f97cd1fafba40b989364c
mnm109 <61045251+mnm109@users.noreply.github.com>
2020-02-26 02:31:13 +0900

    Update messages_controller_spec.rb

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+    context 'log in' do
+    end
+
+    context 'not log in' do
+    end
+    
+  end
+end
