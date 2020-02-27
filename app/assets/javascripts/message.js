b803b6ba68bf5807f240efa37f1bc38ad83522c6
mnm109 <61045251+mnm109@users.noreply.github.com>
2020-02-27 12:34:56 +0900

    Update message.js
    綴り、クラスの修正
    doneとfailの追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,18 +2,18 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
+        `<div class="main__chat__message" data-message-id=${message.id}>
+          <div class="main__chat__message__info">
+            <div class="main__chat__message__info__member">
               ${message.user_name}
             </div>
-            <div class="upper-message__date">
+            <div class="main__chat__message__info__datetime">
               ${message.created_at}
             </div>
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
+          <div class="main__chat__message__text">
+            <p class="main__chat__message__text__content">
+              ${meaasage.content}
             </p>
           </div>
           <img src=${message.image} >
@@ -21,17 +21,17 @@ $(function(){
       return html;
     } else {
       var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
+        `<div class="main__chat__message" data-message-id=${message.id}>
+          <div class="main__chat__message__info">
+            <div class="main__chat__message__info__member">
               ${message.user_name}
             </div>
-            <div class="upper-message__date">
+            <div class="main__chat__message__info__datetime">
               ${message.created_at}
             </div>
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
+          <div class="main__chat__message__text">
+            <p class="main__chat__message__text__content">
               ${message.content}
             </p>
           </div>
@@ -45,11 +45,23 @@ $(function(){
     var url = $(this).attr('action');
     $.ajax({
       url: url,
-      type: 'POST',
+      type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
-  });
+      .done(function(data){
+        console.log(data)
+        var html = buildHTML(data);
+        $('.main__chat__messages').append(html);
+        $('.main__chat__messages').animate({ scrollTop: $('.main__chat__messages')[0].scrollHeight});
+        $('form')[0].reset();
+        $('.submit_btn').prop('disabled', false);
+      })
+      .fail(function(){
+        alert("メッセージ送信に失敗しました");
+        $('.submit_btn').prop('disabled', false);
+      })
+  })
 });
