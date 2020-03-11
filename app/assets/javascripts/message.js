c1af427c2a528818b5bb5b2facc6d8a76a542ef9
mnm109 <61045251+mnm109@users.noreply.github.com>
2020-03-11 20:46:46 +0900

    fin
    自動更新エラー部分修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -74,9 +74,11 @@ $(function(){
       .fail(function(){
         alert("メッセージ送信に失敗しました");
         $('.submit_btn').prop('disabled', false);
-      })
+      });
+    })
+
     var reloadMessages = function() {
-      var last_message_id = $('.message:last').data("message-id");
+      var last_message_id = $('.main__chat__message:last').data("message-id");
       $.ajax({
         url: "api/messages",
         type: 'get',
@@ -89,15 +91,15 @@ $(function(){
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        $('.messages').append(insertHTML);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.main__chat__messages').append(insertHTML);
+        $('.main__chat__messages').animate({ scrollTop: $('.main__chat__messages')[0].scrollHeight});
         }
       })
       .fail(function() {
         alert('error');
       });
     };
-  });
+
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
   setInterval(reloadMessages, 7000);
   }
