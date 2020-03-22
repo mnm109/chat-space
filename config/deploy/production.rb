df7ceb20391494946bf8c1fa7ae391783a4d1e1c
mnm109 <61045251+mnm109@users.noreply.github.com>
2020-03-23 00:28:50 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '18.178.64.253', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
